a1824aa3d06648443239ad179699d520c762731d
Dmitry <kiselev_d@fdoctor.ru>
2022-12-09 12:50:51 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Success from './components/Success';
+import Login from './components/Login';
+import RequireAuth from './components/RequireAuth';
+import { WebAuthProvider } from './hooks/useWebAuth';
+import './App.scss';
 
-function App() {
+const App: React.FC<{}> = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WebAuthProvider>
+      <BrowserRouter>
+        <div className="App">
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/success" element={<RequireAuth>
+              <Success />
+            </RequireAuth>}
+            />
+            <Route
+              path="*"
+              element={<Navigate to="/success" replace />}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </WebAuthProvider>
   );
 }
 
